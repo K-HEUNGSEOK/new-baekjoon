d00ff74971370067fc434f4d8ad5cb56ae8c98b8
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-08-15 12:21:59 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 212 ms, Memory: 21648 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -6,37 +6,28 @@ import java.util.List;
 
 public class Main {
     static int n, k;
-    static int[] visit;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
-        
+     
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         k = sc.nextInt();
-        visit = new int[n + 1];
-        visit[0] = 1;
-        BFS(0);
-        if (visit[n] != 0 && visit[n] -1 <= k){
+        dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 0; i < n ; i++){
+            if (i + 1 <= n ){
+                dp[i+1] = Math.min(dp[i] + 1, dp[i+1]);
+            }
+            if (i + i/2 <= n){
+                dp[i+ i/2] = Math.min(dp[i+i/2], dp[i] + 1);
+            }
+        }
+
+        if (dp[n] <= k){
             System.out.println("minigimbob");
         }else{
             System.out.println("water");
-        }
-    }
-    static void BFS(int v){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(v);
-        while (!queue.isEmpty()){
-            int temp = queue.poll();
-
-            int tempA = temp + 1;
-            if (tempA <= n && visit[tempA] == 0){
-                queue.offer(tempA);
-                visit[tempA] = visit[temp] + 1;
-            }
-            int tempB = temp + (temp/2);
-            if (tempB <= n && visit[tempB] == 0){
-                queue.offer(tempB);
-                visit[tempB] = visit[temp] + 1;
-            }
         }
     }
 }
