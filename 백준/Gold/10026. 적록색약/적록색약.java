fc30a881602bc4c883db1bbc5b5dc421354b0de7
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-08-20 12:16:28 +0900

    [Gold V] Title: 적록색약, Time: 228 ms, Memory: 20732 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,3 +1,4 @@
+import javax.print.DocFlavor;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
@@ -12,7 +13,7 @@ public class Main {
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
     public static void main(String[] args) throws IOException {
-       
+     
         Scanner sc = new Scanner(System.in);
         //적록 색약인사람 R이랑 G랑 같음
         //일반인은 다 구별함
@@ -29,7 +30,7 @@ public class Main {
             for(int j = 0 ; j < n ; j ++){
                 if (!ch[i][j]){
                     person ++;
-                    DFS(i,j);
+                    DFS(i,j, false);
                 }
             }
         }
@@ -38,45 +39,34 @@ public class Main {
             for(int j = 0 ; j < n ; j ++){
                 if (!ch[i][j]){
                     colorPerson ++;
-                    DFS2(i,j);
+                    DFS(i,j,true);
                 }
             }
         }
         System.out.println(person + " " + colorPerson);
     }
-    static void DFS2(int x, int y){
-        ch[x][y] = true;
+    static void DFS(int x, int y , boolean check){
         char current = graph[x][y];
         for(int i = 0 ; i < 4; i ++){
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (isRange(nx,ny) && !ch[nx][ny]){
-                if (current == 'R' || current == 'G'){
-                    if (graph[nx][ny] == 'R' || graph[nx][ny] == 'G'){
-                        ch[nx][ny] = true;
-                        DFS2(nx,ny);
-                    }
-                }else{
-                    if (current == graph[nx][ny]){
-                     ch[nx][ny] = true;
-                     DFS2(nx,ny);
-                    }
+                if (isSame(current, graph[nx][ny], check)){
+                    ch[nx][ny] = true;
+                    DFS(nx,ny,check);
                 }
             }
         }
     }
-    static void DFS(int x, int y){
-        ch[x][y] = true;
-        char current = graph[x][y];
-        for(int i = 0 ; i < 4; i ++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (isRange(nx,ny) && !ch[nx][ny] && graph[nx][ny] == current){
-                ch[nx][ny] = true;
-                DFS(nx,ny);
+    static boolean isSame(char a, char b, boolean check){
+        if (check){ // 트루면 색약임
+            if ((a == 'R' || a == 'G') && (b == 'R' || b == 'G')){
+                return true;
             }
         }
+        return a == b;
     }
+
     static boolean isRange(int x, int y){
         return 0 <= x && x < n && 0 <= y && y < n;
     }
