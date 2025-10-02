26dd472288915159e4b61af1f541268f5907b698
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-02 15:44:29 +0900

    [D3] Title: 부분 수열의 합, Time: 117 ms, Memory: 28,160 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,41 +1,36 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
-import java.lang.reflect.Array;
+import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
-    static int n,k;
-    static int[] arr;
-    static int ans ;
+    static int n, k,ans;
+    static int[] arr ;
     public static void main(String[] args) throws FileNotFoundException {
-     
+        
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for(int z = 1 ; z <= T ; z++){
-        n = sc.nextInt();
-        k = sc.nextInt();
-        arr = new int[n];
-        ans = 0;
-        for(int i = 0; i < n ; i ++){
-            arr[i] = sc.nextInt();
-        }
-        DFS(0,0);
-        System.out.println("#" + z + " "  + ans);
+        for(int z = 1 ; z<= T;  z++){
+            n = sc.nextInt();
+            k = sc.nextInt();
+            ans = 0;
+            arr = new int[n];
+            for(int i = 0 ; i < n ; i++) arr[i] = sc.nextInt();
+            DFS(0, 0);
+            System.out.println("#" + z + " " + ans);
         }
     }
     static void DFS(int v, int total){
-        if (total > k) return;
-        if (total == k){
-            ans ++;
+        if(total > k) return;
+        if(v == n){
+            if(total == k) ans ++;
             return;
         }
-        if (v == n){
-            return;
-        }else{
-            DFS(v + 1, total + arr[v]);
-            DFS(v + 1, total);
+        else {
+            DFS(v + 1 , total + arr[v]);
+            DFS(v + 1 , total);
         }
     }
 }
+
 
