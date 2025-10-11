151f9beeb5ca6ff49399e34e561aac718eac621d
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-11 14:10:54 +0900

    [D2] Title: 파리 퇴치, Time: 125 ms, Memory: 29,976 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,41 +1,43 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-       
+        
         Scanner sc = new Scanner(System.in);
-
         int T = sc.nextInt();
-        for(int z = 1; z <= T ; z ++) {
+        for (int z = 1; z <= T; z++) {
             int n = sc.nextInt();
             int m = sc.nextInt();
-            int[][] arr = new int[n+1][n+1];
-            for(int i = 1 ; i <= n ; i ++){
-                for(int j = 1 ; j <= n ; j ++) arr[i][j] = sc.nextInt();
-            }
-            int[][] prefix = new int[n+1][n+1];
-            int ans = 0;
-            for(int i = 1 ; i <= n ; i ++){
-                for(int j = 1; j <= n ; j ++){
-                    prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j];
+            int[][] arr = new int[n][n];
+            for(int i = 0 ; i < n ; i ++){
+                for(int j = 0 ; j < n ; j ++){
+                    arr[i][j] = sc.nextInt();
                 }
             }
-            ans = Math.max(ans, prefix[m][m] - prefix[m-1][m] - prefix[m][m-1] + prefix[m-1][m-1]);
 
-            for(int i = 1; i <= n ; i ++){
-                for(int j = 1; j <= n ; j ++){
-                    int x = i - m;
-                    int y = j - m;
-                    if (!isRange(x,y,n)) continue;
-                    ans = Math.max(ans , prefix[i][j] - prefix[x][j] - prefix[i][y] + prefix[x][y]);
+            int ans = 0;
+
+            for(int i = 0 ; i <= n-m ; i ++){
+                for(int j = 0 ; j <= n-m ; j ++){
+                    int total = 0 ;
+                    for(int k = i; k < i + m; k ++){
+                        for(int l = j ; l < j + m; l ++){
+                            total += arr[k][l];
+                        }
+                    }
+                    ans = Math.max(ans,total);
                 }
             }
-            System.out.println("#" + z + " " +  ans);
+
+            System.out.println("#" + z + " " + ans);
         }
     }
-    static boolean isRange(int x, int y, int n){
-        return 0 <= x && x <= n && 0 <= y && y <= n;
+    static boolean isRange(int x, int y , int n ){
+        return 0 <= x && x < n && 0 <=y && y < n;
     }
 }
+
+
