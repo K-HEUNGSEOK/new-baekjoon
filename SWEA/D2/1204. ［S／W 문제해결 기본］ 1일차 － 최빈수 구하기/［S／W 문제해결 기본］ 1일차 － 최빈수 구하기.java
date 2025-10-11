a78ac914c93f31428261ea1e3c6a735e95ea7144
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-11 13:42:00 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 151 ms, Memory: 40,732 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,31 +1,31 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-
+        
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for(int z = 1 ; z <= T ; z ++){
-            int index = sc.nextInt();
-            int findNumber = 0;
-            int cnt = 0;
-            Map<Integer, Integer> map = new HashMap<>();
+        for (int z = 1; z <= T; z++) {
+            int[] arr = new int[101];
+            int f = sc.nextInt();
             for(int i = 0 ; i < 1000; i ++){
                 int num = sc.nextInt();
-                map.put(num, map.getOrDefault(num, 0) +1);
+                arr[num] ++;
             }
-
-            for (Integer i : map.keySet()) {
-                if (map.get(i) > cnt){
-                    cnt = map.get(i);
-                    findNumber = i;
-                }else if (map.get(i) == cnt){
-                    findNumber = Math.max(findNumber,i);
+            int ans = 0;
+            int index = 0 ;
+            for(int i = 0 ; i <= 100; i ++){
+                if(arr[i] >= ans){
+                    ans = arr[i];
+                    index = i;
                 }
             }
-            System.out.println("#"+index + " " + findNumber);
+            System.out.println("#" + z +" " + index);
         }
     }
 }
+
+
