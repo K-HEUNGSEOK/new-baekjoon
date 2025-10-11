e5d2f870bd18c495fa5bd4e4ce09192bc2ac6138
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-11 14:35:26 +0900

    [D2] Title: 간단한 369게임, Time: 102 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,44 +1,38 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-   
+    
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n ; i ++){
-            if (checkNumber(i,sb)){
 
-            }else{
-                sb.append(i).append(" ");
+            int n = sc.nextInt();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 1; i <= n ; i ++){
+                int cnt = check(i);
+                if(cnt == 0){
+                    sb.append(i).append(" ");
+                }else{
+                    for(int j = 0 ; j < cnt; j ++){
+                        sb.append("-");
+                    }
+                    sb.append(" ");
+                }
             }
-        }
-        System.out.println(sb.toString());
+            System.out.println(sb.toString());
+
     }
-    static boolean checkNumber(int num, StringBuilder sb){
-        char[] arr = String.valueOf(num).toCharArray();
-        //횟수를 세서 그만큼 - 넣으면 되잖아
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
-        for (char c : arr) {
-            if (c - '0' == 3) cnt1 ++;
-            else if(c-'0' == 6) cnt2 ++;
-            else if(c-'0' == 9) cnt3 ++;
-        }
-        if (cnt1 == 0 && cnt2 == 0 && cnt3 ==0) return false;
-        int total = cnt1 + cnt2 + cnt3;
+    static int check(int n){
+        int cnt = 0;
+        char[] a  = (n +"").toCharArray();
 
-        if (total > 1){
-            for(int i = 0 ; i < total; i ++){
-                sb.append('-');
-            }
-            sb.append(" ");
-        }else{
-            sb.append('-').append(" ");
+        for(char x : a){
+            if(x == '3' || x == '6' || x == '9') cnt ++;
         }
-        return true;
+        return cnt;
     }
 }
+
+
