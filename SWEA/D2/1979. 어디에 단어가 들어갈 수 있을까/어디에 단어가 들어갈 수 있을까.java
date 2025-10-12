604b09f971d20e8ef3c9072cdee6416ae2efc760
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-12 23:25:03 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 125 ms, Memory: 30,536 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,60 +1,58 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
-    static StringBuilder sb;
-
     public static void main(String[] args) throws FileNotFoundException {
-       
+  
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for (int z = 1; z <= T; z++) {
-            int ans = 0;
+
+        for(int z = 1 ; z <= T ; z ++){
             int n = sc.nextInt();
-            int k = sc.nextInt();
+            int m = sc.nextInt();
             int[][] arr = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+            for(int i = 0 ; i < n; i ++){
+                for(int j = 0 ; j < n ; j ++){
                     arr[i][j] = sc.nextInt();
                 }
             }
 
-            for(int i = 0 ; i < n ; i ++){
-                int cnt = 0;
-                for(int j = 0 ; j < n ; j ++){
-                    if (arr[i][j] == 1) cnt ++;
-                    else {
-                        if (cnt == k){
-                            ans ++;
-                        }
-                        cnt = 0;
-                    }
-                }
-                if (cnt == k) {
-                   ans ++;
-                }
-            }
+            int ans = 0 ;
 
             for(int i = 0 ; i < n ; i ++){
                 int cnt = 0;
                 for(int j = 0 ; j < n ; j ++){
-                    if (arr[j][i] == 1) cnt ++;
-                    else {
-                        if (cnt == k){
-                            ans ++;
-                        }
+                    if(arr[i][j] == 1){
+                        cnt ++;
+                    }else{
+                        if(cnt == m) ans ++;
                         cnt = 0;
                     }
                 }
-                if (cnt == k) {
+                if(cnt == m){
                     ans ++;
                 }
             }
-
-
-            //1.먼저 1일때 를 센다. 열과 행을 이때 cnt가 하나라도 k개와 같다면 ans 를 올리는 것
+            for(int i = 0 ; i < n ; i ++){
+                int cnt = 0;
+                for(int j = 0 ; j < n ; j ++){
+                    if(arr[j][i] == 1){
+                        cnt +=1;
+                    }else{
+                        if(cnt == m) ans ++;
+                        cnt = 0;
+                    }
+                }
+                if(cnt == m){
+                    ans ++;
+                }
+            }
             System.out.println("#" + z + " " + ans);
+
         }
     }
 }
+
+
