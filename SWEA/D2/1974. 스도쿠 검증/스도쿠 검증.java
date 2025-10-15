192441e61d845f4434314db461457d97d292d277
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-15 22:31:39 +0900

    [D2] Title: 스도쿠 검증, Time: 104 ms, Memory: 27,392 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,65 +1,97 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-       
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int z = 1; z <= T; z ++) {
-            int n = 9;
-            boolean flag = true;
-            int[][] arr = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-            for(int i = 0 ; i < n ; i ++){
-                int[] width = new int[10];
-                int[] height = new int[10];
-                for(int j = 0 ; j < n ; j ++){
-                    width[arr[i][j]] ++;
-                    height[arr[j][i]] ++;
-                }
 
-                for(int k = 1; k < 10; k ++){
-                    if (width[k] != 1 || height[k]!= 1){
-                        flag = false;
-                        break;
-                    }
-                }
-            }
+  static int n = 9;
+  static int m = 3;
 
+  public static void main(String[] args) throws FileNotFoundException {
 
-            if (flag) {
-                for (int i = 0; i < n; i += 3) {
-                    for (int j = 0; j < n; j += 3) {
-                        int[] check = new int[10];
-                        for (int k = i; k < i + 3; k++) {
-                            for (int l = j; l < j + 3; l++) {
-                                if (k > n) continue;
-                                int index = arr[k][l];
-                                check[index]++;
-                            }
-                        }
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
 
-                        for(int ch = 1; ch <= 9; ch ++){
-                            if (check[ch] != 1){
-                                flag = false;
-                                break;
-                            }
-                        }
-                        if (!flag) break;
-                    }
-                }
-            }
-
-
-            System.out.print("#" + z +" ");
-            if (flag) System.out.println(1);
-            else System.out.println(0);
+    for (int z = 1; z <= T; z++) {
+      int[][] arr = new int[n][n];
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          arr[i][j] = sc.nextInt();
         }
+      }
+      //1. 가로를 다 확인한다.
+      //2. 세로를 다 확인한다.
+      //3. 3*3으로 확인한다.
+
+      System.out.print("#" + z + " ");
+      if (execute(arr)) {
+        System.out.println(1);
+      } else {
+        System.out.println(0);
+      }
     }
+  }
+
+  static boolean execute(int[][] arr) {
+    if (!widthCheck(arr)) {
+      return false;
+    }
+    if (!heightCheck(arr)) {
+      return false;
+    }
+
+    for (int i = 0; i <= n - m; i += 3) {
+      for (int j = 0; j <= n - m; j += 3) {
+        int[] temp = new int[10];
+        for (int k = i; k < i + 3; k++) {
+          for (int l = j; l < j + 3; l++) {
+            int num = arr[k][l];
+            temp[num]++;
+          }
+        }
+        for (int z = 1; z <= 9; z++) {
+          if (temp[z] != 1) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  static boolean widthCheck(int[][] arr) {
+    for (int i = 0; i < n; i++) {
+      int[] temp = new int[10];
+      for (int j = 0; j < n; j++) {
+        int num = arr[i][j];
+        temp[num]++;
+      }
+
+      for (int k = 1; k <= n; k++) {
+        if (temp[k] != 1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  static boolean heightCheck(int[][] arr) {
+    for (int i = 0; i < n; i++) {
+      int[] temp = new int[10];
+      for (int j = 0; j < n; j++) {
+        int num = arr[j][i];
+        temp[num]++;
+      }
+
+      for (int k = 1; k <= n; k++) {
+        if (temp[k] != 1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
+
+
