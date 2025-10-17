ee9fb7c7edd927d97056048cd11edb8b131fdac6
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-17 10:52:17 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 153 ms, Memory: 39,052 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,35 +1,35 @@
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
-import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        int T = 10;
-        int ans = 1;
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            //맨왼쪽 두칸과 맨오른쪽 두칸은 건물을 못지음
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-            }
-            int sum = 0;
-            for (int i = 2; i < n - 2; i++) {
-                if (arr[i] <= 0) continue;
+  public static void main(String[] args) throws FileNotFoundException {
+  
+    Scanner sc = new Scanner(System.in);
 
-                if (arr[i - 1] < arr[i] && arr[i - 2] < arr[i] && arr[i] > arr[i + 1] && arr[i] > arr[i + 2]) {
-                    int max = Math.max(arr[i - 1], arr[i - 2]);
-                    max = Math.max(max, arr[i + 1]);
-                    max = Math.max(max, arr[i + 2]);
-                    sum += arr[i] - max;
-                }
-            }
-            System.out.println("#" + ans++ + " " + sum);
+    int T = 10;
+    for (int z = 1; z <= T; z++) {
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      for (int i = 0; i < n; i++) {
+        arr[i] = sc.nextInt();
+      }
+
+      int ans = 0;
+
+      for (int i = 2; i < n - 2; i++) {
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i - 2] && arr[i] > arr[i + 1] && arr[i] > arr[i
+            + 2]) {
+          int maxA = Math.max(arr[i - 1], arr[i - 2]);
+          int maxB = Math.max(arr[i + 1], arr[i + 2]);
+          int result = Math.max(maxA, maxB);
+
+          ans += arr[i] - result;
         }
+      }
+
+      System.out.println("#" + z + " " + ans);
     }
+  }
 }
