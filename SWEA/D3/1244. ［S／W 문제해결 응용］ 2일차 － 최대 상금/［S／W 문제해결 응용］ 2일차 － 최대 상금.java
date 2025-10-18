8460466c494a9c186b2f52ab4232a8dc58d41ebf
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-18 13:03:00 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 153 ms, Memory: 92,084 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,61 +1,61 @@
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
-import java.io.*;
 
 public class Solution {
-    static int max;
-    static boolean[][] visited;
-    public static void main(String[] args) throws IOException {
-      
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
-        for(int start = 1 ; start <= T; start ++){
-            String str = sc.next(); //처음 들어오는 숫자
-            int change = sc.nextInt(); //교환횟수
 
-            max = 0; // 최대값 갱신용도
-            visited = new boolean[change + 1][1000000]; //몇번의 기회인지와 숫자의 최대값 작성
+  static char[] arr;
+  static int c;
+  static int ans;
+  static boolean[][] visited;
 
-            int[] arr = new int[str.length()]; //바꾸기 쉽게 int 배열로 생성
-            for(int i = 0 ; i < arr.length; i ++){
-                arr[i] = str.charAt(i)-'0';
-            }
+  public static void main(String[] args) throws FileNotFoundException {
 
-            //교환 횟수, 배열, 교환해야하는 횟수, 처음 값 집어넣고 조합을 돌린다.
-            DFS(0,arr, change,Integer.parseInt(str));
-            sb.append("#" + start + " " + max).append("\n");
-        }
-        System.out.println(sb.toString());
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int z = 1; z <= T; z++) {
+      String str = sc.next();
+      ans = 0;
+      c = sc.nextInt();
+      arr = new char[str.length()];
+      for (int i = 0; i < str.length(); i++) {
+        arr[i] = str.charAt(i);
+      }
+      visited = new boolean[c + 1][1000000];
+
+      DFS(0, Integer.parseInt(str));
+      System.out.println("#" + z + " " + ans);
     }
-    static void DFS(int v, int[] arr,int change, int current){
-        if (visited[v][current]) return;
-        visited[v][current] = true;
-        if (v == change){
-            if (current > max){
-                max = current;
-                return;
-            }
-        }else{
-            for(int i = 0 ; i < arr.length; i ++){
-                for(int j =  i + 1; j < arr.length; j ++){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    StringBuilder sb = new StringBuilder();
-                    for (int i1 : arr) {
-                        sb.append(i1);
-                    }
+  }
 
-                    DFS(v + 1, arr, change, Integer.parseInt(sb.toString()));
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+  static void DFS(int v, int current) {
+    if (visited[v][current]) {
+      return;
     }
+    visited[v][current] = true;
+    if (v == c) {
+      if (ans < current) {
+        ans = current;
+      }
+      return;
+
+    } else {
+      for (int i = 0; i < arr.length; i++) {
+        for (int j = i + 1; j < arr.length; j++) {
+          swap(i, j);
+
+          DFS(v + 1, Integer.parseInt(new String(arr)));
+
+          swap(i, j);
+        }
+      }
+    }
+  }
+
+  static void swap(int i, int j) {
+    char temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 }
