3b0e0d0a69a903475887acd3e51b79798155915a
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-19 13:29:22 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 157 ms, Memory: 91,956 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -4,50 +4,46 @@ import java.util.Scanner;
 
 public class Solution {
 
-  static char[] arr;
-  static int c;
-  static int ans;
+  static int ans, cnt;
   static boolean[][] visited;
+  static char[] arr;
 
   public static void main(String[] args) throws FileNotFoundException {
-
+  
     Scanner sc = new Scanner(System.in);
-
+    //1. 재귀 완전탐색으로 가장 큰 수를 찾는다
+    //2. 백트래킹으로 다시 되돌려놔야 한다
+    //3. 방문 체크를 해서 가지치기를 하자
     int T = sc.nextInt();
     for (int z = 1; z <= T; z++) {
       String str = sc.next();
       ans = 0;
-      c = sc.nextInt();
+      cnt = sc.nextInt();
       arr = new char[str.length()];
       for (int i = 0; i < str.length(); i++) {
         arr[i] = str.charAt(i);
       }
-      visited = new boolean[c + 1][1000000];
-
+      visited = new boolean[cnt + 1][1000000]; //최대 숫자가 99만임
       DFS(0, Integer.parseInt(str));
       System.out.println("#" + z + " " + ans);
     }
+
   }
 
-  static void DFS(int v, int current) {
-    if (visited[v][current]) {
+  static void DFS(int v, int total) {
+    if (visited[v][total]) {
       return;
     }
-    visited[v][current] = true;
-    if (v == c) {
-      if (ans < current) {
-        ans = current;
-      }
+    visited[v][total] = true;
+    if (v == cnt) {
+      ans = Math.max(ans, total);
       return;
-
     } else {
       for (int i = 0; i < arr.length; i++) {
         for (int j = i + 1; j < arr.length; j++) {
-          swap(i, j);
-
+          swap(i, j); //스왑하기
           DFS(v + 1, Integer.parseInt(new String(arr)));
-
-          swap(i, j);
+          swap(i, j); //다시 풀어주기
         }
       }
     }
