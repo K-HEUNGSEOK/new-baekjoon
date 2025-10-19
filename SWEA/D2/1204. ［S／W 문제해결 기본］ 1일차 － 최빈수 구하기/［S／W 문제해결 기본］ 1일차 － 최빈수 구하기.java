2ae16dfc4b84bfff7f3ee941ff83111f9ac90665
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-19 14:05:45 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 158 ms, Memory: 40,252 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,31 +1,30 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-        
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int z = 1; z <= T; z++) {
-            int[] arr = new int[101];
-            int f = sc.nextInt();
-            for(int i = 0 ; i < 1000; i ++){
-                int num = sc.nextInt();
-                arr[num] ++;
-            }
-            int ans = 0;
-            int index = 0 ;
-            for(int i = 0 ; i <= 100; i ++){
-                if(arr[i] >= ans){
-                    ans = arr[i];
-                    index = i;
-                }
-            }
-            System.out.println("#" + z +" " + index);
+
+  public static void main(String[] args) throws FileNotFoundException {
+
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int z = 1; z <= T; z++) {
+      int[] arr = new int[101];
+      int g = sc.nextInt();
+      for (int i = 0; i < 1000; i++) {
+        int num = sc.nextInt();
+        arr[num]++;
+      }
+      int max = 0;
+      int cnt = 0;
+
+      for (int i = 0; i <= 100; i++) {
+        if (arr[i] >= cnt) {
+          cnt = arr[i];
+          max = i;
         }
+      }
+      System.out.println("#" + z + " " + max);
     }
+  }
 }
-
-
