5cda708527daae5ae555625ad9a407f4dbf93ce8
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-22 14:26:35 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 115 ms, Memory: 28,968 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
@@ -1,69 +1,98 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
 
 public class Solution {
-    static String [] num = {"0001101", "0011001", "0010011", "0111101", "0100011","0110001","0101111","0111011","0110111","0001011"};
-    public static void main(String[] args) throws FileNotFoundException {
-    
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int z = 1; z <= T; z++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char[][] arr = new char[n+1][m+1];
-            int x = 0;
-            int y = 0;
-            for(int i = 0 ; i < n ; i ++){
-                String str = sc.next();
-                for(int j = 0 ; j < m; j ++){
-                    arr[i][j] = str.charAt(j);
-                    if (arr[i][j] == '1'){
-                        x = i;
-                        y = j;
-                    }
-                }
-            }
-            StringBuilder sb = new StringBuilder();
 
-            for(int i = y ; i > y - 56; i --){
-                sb.append(arr[x][i]);
-            }
-            String str = sb.reverse().toString();
-
-            StringBuilder ans = new StringBuilder();
-            for(int i = 0 ; i < str.length(); i += 7){
-                String sub = str.substring(i, i+7);
-                for(int j = 0 ; j < num.length; j ++){
-                    if (num[j].equals(sub)){
-                        ans.append(j);
-                        break;
-                    }
-                }
-            }
-            String temp = ans.toString();
-            int ans1 = 0; //짝수
-            int ans2 = 0; //홀수
-            int total = 0;
-            for(int i = 0 ; i < temp.length(); i ++){
-                if (i % 2 == 0){
-                    ans1 += (temp.charAt(i) -'0');
-                }else{
-                    ans2 += (temp.charAt(i) -'0');
-                }
-
-                total += (temp.charAt(i)-'0');
-            }
-            ans1 *= 3;
-
-            if ((ans1 + ans2) % 10 == 0){
-                System.out.println("#" + z + " " + total);
-            }else{
-                System.out.println("#" + z + " " + 0);
-            }
+  public static void main(String[] args) throws FileNotFoundException {
+   
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int z = 1; z <= T; z++) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      char[][] arr = new char[n][m];
+      for (int i = 0; i < n; i++) {
+        arr[i] = sc.next().toCharArray();
+      }
+      int x = 0;
+      int y = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if (arr[i][j] == '1') {
+            x = i;
+            y = j;
+          }
         }
-    }
-}
+      }
+      StringBuilder sb = new StringBuilder();
+      for (int i = y - 55; i <= y; i++) {
+        sb.append(arr[x][i]);
+      }
 
+      String str = sb.toString();
+      StringBuilder ans = new StringBuilder();
+
+      int len = str.length();
+
+      for (int i = 0; i < len; i += 7) {
+        String temp = str.substring(i, i + 7);
+        ans.append(getNumber(temp));
+      }
+
+      String s = ans.toString();
+      int total = 0;
+      for (int i = 0; i < s.length(); i++) {
+        if (i % 2 == 0) {
+          total += s.charAt(i) - '0';
+        }
+      }
+      total *= 3;
+      for (int i = 0; i < s.length(); i++) {
+        if (i % 2 != 0) {
+          total += s.charAt(i) - '0';
+        }
+      }
+
+      int result = 0;
+      for (int i = 0; i < s.length(); i++) {
+        result += s.charAt(i) - '0';
+      }
+
+      System.out.print("#" + z + " ");
+      if (total % 10 == 0) {
+        System.out.print(result);
+      } else {
+        System.out.print(0);
+      }
+      System.out.println();
+    }
+  }
+
+  static int getNumber(String str) {
+    switch (str) {
+      case "0001101":
+        return 0;
+      case "0011001":
+        return 1;
+      case "0010011":
+        return 2;
+      case "0111101":
+        return 3;
+      case "0100011":
+        return 4;
+      case "0110001":
+        return 5;
+      case "0101111":
+        return 6;
+      case "0111011":
+        return 7;
+      case "0110111":
+        return 8;
+      case "0001011":
+        return 9;
+    }
+    return -1;
+  }
+}
 
