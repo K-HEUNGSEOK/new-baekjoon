7ad6ecd9d3099e5c0b45c7c4bf1bff5bed5c7ce4
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-22 13:35:23 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 116 ms, Memory: 29,056 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,46 +1,44 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
-import java.util.*;
+
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-  
-        Scanner sc = new Scanner(System.in);
-        int T = 10;
-        for (int z = 1; z <= T; z++) {
-            int remove = sc.nextInt();
-            int n = 8;
-            Deque<Integer> list = new ArrayDeque<>();
-            for(int i = 0 ; i < 8; i ++){
-                list.add(sc.nextInt());
-            }
-            int cnt = 1;
-            while (true){
-                Integer pop = list.pop();
 
-                pop -= cnt;
-                if (pop <= 0){
-                    list.add(0);
-                    break;
-                }
-                list.add(pop);
-                cnt ++;
-                if (cnt > 5){
-                    cnt = 1;
-                }
+  static int n = 8;
 
-            }
-
-
-            //출력
-            System.out.print("#" + z + " " );
-            for (Integer i : list) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-
+  public static void main(String[] args) throws FileNotFoundException {
+ 
+    Scanner sc = new Scanner(System.in);
+    int T = 10;
+    for (int z = 1; z <= T; z++) {
+      int index = sc.nextInt();
+      Deque<Integer> deque = new ArrayDeque<>();
+      for (int i = 0; i < n; i++) {
+        deque.offer(sc.nextInt());
+      }
+      int cnt = 1;
+      while (true) {
+        Integer pop = deque.pop();
+        pop -= cnt;
+        cnt++;
+        if (cnt > 5) {
+          cnt = 1;
         }
+        if (pop <= 0) {
+          pop = 0;
+          deque.offer(pop);
+          break;
+        }
+        deque.offer(pop);
+      }
+      System.out.print("#" + index + " ");
+      for (Integer i : deque) {
+        System.out.print(i + " ");
+      }
+      System.out.println();
     }
+  }
 }
-
 
