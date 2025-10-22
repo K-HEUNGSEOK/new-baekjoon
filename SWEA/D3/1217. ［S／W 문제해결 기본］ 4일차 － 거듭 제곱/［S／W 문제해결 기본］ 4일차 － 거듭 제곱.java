ff33dd5af47f20745749efe20934f4dfbcc302c9
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-22 15:10:53 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 89 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,24 +1,33 @@
-import java.util.Scanner;
-public class Solution {
-    public static void main(String[] args)  {
-      
-        Scanner sc = new Scanner(System.in);
-        int T = 10;
-        for (int z = 1; z <= T; z++) {
-            int v =sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
 
-            int ans = pow(a,b);
-            System.out.println("#" + z + " " + ans);
-        }
+import java.util.Scanner;
+
+public class Solution {
+
+  static int n;
+  static int ans;
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int T = 10;
+    for (int z = 1; z <= T; z++) {
+      int trash = sc.nextInt();
+      n = sc.nextInt();
+      int m = sc.nextInt();
+      ans = 1;
+      //9 * 9 를 계속 하는거임
+      DFS(m);
+      System.out.println("#" + z + " " + ans);
     }
-    static int pow(int a, int b){
-        if (b == 0){
-            return 1;
-        }else{
-            return a * pow(a,b-1);
-        }
+  }
+
+  static void DFS(int v) {
+    if (v == 0) {
+      return;
+    } else {
+      ans *= n;
+      DFS(v - 1);
     }
+  }
 }
 
