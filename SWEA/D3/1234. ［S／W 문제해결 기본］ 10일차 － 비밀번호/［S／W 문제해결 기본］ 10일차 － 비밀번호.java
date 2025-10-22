ec58d6f1fc44e5752b1036d250f1378f2c5bdcba
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-22 15:02:48 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 116 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -1,38 +1,38 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-   
-        Scanner sc = new Scanner(System.in);
-        int T = 10;
-        for (int z = 1; z <= T; z++) {
-            String r = sc.nextLine();
-            String[] split = r.split(" ");
-            int n = Integer.parseInt(split[0]);
-            String str = split[1];
 
-            Stack<Integer> stack = new Stack<>();
-            for(int i = 0 ; i < n ; i ++){
-                int current = str.charAt(i) -'0';
+  public static void main(String[] args) throws FileNotFoundException {
+  
+    Scanner sc = new Scanner(System.in);
+    int T = 10;
+    for (int z = 1; z <= T; z++) {
+      StringTokenizer st = new StringTokenizer(sc.nextLine());
+      int n = Integer.parseInt(st.nextToken());
+      char[] arr = st.nextToken().toCharArray();
 
-                if (!stack.isEmpty() && stack.peek() == current){
-                    stack.pop();
-                }else{
-                    stack.push(current);
-                }
-            }
-
-
-            System.out.print("#" + z + " ");
-            for (Integer i : stack) {
-                System.out.print(i);
-            }
-            System.out.println();
+      Stack<Integer> stack = new Stack<>();
+      for (int i = 0; i < n; i++) {
+        if (stack.isEmpty()) {
+          stack.push(arr[i] - '0');
+        } else if (stack.peek() == arr[i] - '0') {
+          stack.pop();
+        } else {
+          stack.push(arr[i] - '0');
         }
+      }
+      StringBuilder sb = new StringBuilder();
+      for (Integer i : stack) {
+        sb.append(i);
+      }
+      System.out.print("#" + z + " ");
+      System.out.print(sb.toString());
+      System.out.println();
     }
+  }
 }
-
 
