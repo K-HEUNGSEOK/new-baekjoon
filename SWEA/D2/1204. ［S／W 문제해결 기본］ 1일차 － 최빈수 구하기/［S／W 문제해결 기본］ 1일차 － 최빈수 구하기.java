f0a6b7c10fc94062c2aeb8e13b870f6d07b4b06d
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-26 22:37:23 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 164 ms, Memory: 40,620 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,30 +1,35 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.util.Scanner;
-
+import java.io.FileInputStream;
+import java.util.*;
 public class Solution {
+	static int n, cnt;
+	static int[] arr;
+	public static void main(String args[]) throws Exception{
 
-  public static void main(String[] args) throws FileNotFoundException {
-
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    for (int z = 1; z <= T; z++) {
-      int[] arr = new int[101];
-      int g = sc.nextInt();
-      for (int i = 0; i < 1000; i++) {
-        int num = sc.nextInt();
-        arr[num]++;
-      }
-      int max = 0;
-      int cnt = 0;
-
-      for (int i = 0; i <= 100; i++) {
-        if (arr[i] >= cnt) {
-          cnt = arr[i];
-          max = i;
-        }
-      }
-      System.out.println("#" + z + " " + max);
-    }
-  }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int z = 1 ; z <= T ; z ++) {
+			int trash = sc.nextInt();
+			int[] arr = new int[101];
+			
+			for(int i = 0 ; i < 1000; i ++) {
+				int num = sc.nextInt();
+				arr[num] ++;
+			}
+			
+			int maxNum = 0;
+			int maxIndex = 0;
+			
+			for(int i = 1; i <= 100; i ++) {
+				if(arr[i] >= maxIndex) {
+					maxNum = Math.max(maxNum, i);
+					maxIndex = Math.max(maxIndex, arr[i]);
+				}
+			}
+			
+			System.out.println("#" + z + " " + maxNum);
+		}
+	}
 }
