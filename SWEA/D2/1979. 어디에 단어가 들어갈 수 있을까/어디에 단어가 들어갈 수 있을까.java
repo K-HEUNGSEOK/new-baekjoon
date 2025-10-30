c6bb01484f2253fec278686fe9a3e945f01ef18d
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-30 21:40:59 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 119 ms, Memory: 30,680 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,58 +1,62 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-  
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for(int z = 1 ; z <= T ; z ++){
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int[][] arr = new int[n][n];
-            for(int i = 0 ; i < n; i ++){
-                for(int j = 0 ; j < n ; j ++){
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-
-            int ans = 0 ;
-
-            for(int i = 0 ; i < n ; i ++){
-                int cnt = 0;
-                for(int j = 0 ; j < n ; j ++){
-                    if(arr[i][j] == 1){
-                        cnt ++;
-                    }else{
-                        if(cnt == m) ans ++;
-                        cnt = 0;
-                    }
-                }
-                if(cnt == m){
-                    ans ++;
-                }
-            }
-            for(int i = 0 ; i < n ; i ++){
-                int cnt = 0;
-                for(int j = 0 ; j < n ; j ++){
-                    if(arr[j][i] == 1){
-                        cnt +=1;
-                    }else{
-                        if(cnt == m) ans ++;
-                        cnt = 0;
-                    }
-                }
-                if(cnt == m){
-                    ans ++;
-                }
-            }
-            System.out.println("#" + z + " " + ans);
-
-        }
-    }
+	public static void main(String args[]) throws Exception {
+	
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int z = 1; z <= T; z++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int ans = 0;
+			//1.입력받기
+			int[][] arr = new int[n][n];
+			for(int i = 0 ; i < n ;  i++) {
+				for(int j = 0 ; j < n ; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			//2.가로 로직
+			for(int i = 0 ; i < n ; i++) {
+				int cnt = 0;
+				for(int j = 0 ; j < n ; j++) {
+					if(arr[i][j] == 0) {
+						if(cnt == m) {
+							ans ++;
+						}
+						cnt = 0;
+					}else {
+						cnt ++;
+					}
+				}
+				if(cnt == m) {
+					ans ++;
+				}
+			}
+		
+			
+			
+			//3.세로 로직
+			for(int i = 0 ; i < n ; i++) {
+				int cnt = 0;
+				for(int j = 0 ; j < n ; j++) {
+					if(arr[j][i] == 0) {
+						if(cnt == m) {
+							ans ++;
+						}
+						cnt = 0;
+					}else {
+						cnt ++;
+					}
+				}
+				if(cnt == m) {
+					ans ++;
+				}
+			}
+		
+			System.out.println("#" + z + " " +ans);
+		}
+	}
 }
-
-
