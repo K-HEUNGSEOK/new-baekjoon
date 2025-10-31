c9c3199ad5fda420ae03bfedcc908c92ae062163
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-10-31 18:29:27 +0900

    [D2] Title: 숫자 배열 회전, Time: 102 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,65 +1,66 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
+	static int n;
+	static StringBuilder sb;
+	static String[][] ans;
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int z = 1; z <= T; z++) {
+			n = sc.nextInt();
+			ans = new String[n][3];
+			sb = new StringBuilder();
+			// 1.입력받기
+			int[][] arr = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			// 2.배열 돌리기
+			for (int f = 0; f < 3; f++) {
+				arr = rotationArray(arr);
+				print(arr);
+			}
+			String[] split = sb.toString().split(" ");
+			
+			int cnt = 0;
+			for(int i = 0 ; i < 3 ; i ++) {
+				for(int j = 0 ; j < n ; j ++) {
+					ans[j][i] = split[cnt++];
+				}
+			}
+			
+			System.out.println("#" + z);
+			for(int i = 0 ; i < n ; i ++) {
+				for(int j = 0 ; j < 3 ; j ++) {
+					System.out.print(ans[i][j] +" ");
+				}
+				System.out.println();
+			}
+		}
+	}
 
-  static int[][] arr;
-  static int[][] copy;
-  static int n;
-  static StringBuilder sb;
+	
+	static void print(int[][] arr) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append(" ");
+		}
+	}
 
-  public static void main(String[] args) throws FileNotFoundException {
-
-    Scanner sc = new Scanner(System.in);
-
-    int T = sc.nextInt();
-    for (int z = 1; z <= T; z++) {
-      sb = new StringBuilder();
-      n = sc.nextInt();
-      arr = new int[n][n];
-      String[][] ans = new String[n][3];
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          arr[i][j] = sc.nextInt();
-        }
-      }
-      //배열을 돌리고 가로의 값을 ans 세로에 각각 넣으면 된다.
-      for (int i = 0; i < 3; i++) {
-        rotation();
-      }
-      String[] str = sb.toString().split(" ");
-      int cnt = 0;
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < n; j++) {
-          ans[j][i] = str[cnt++];
-        }
-      }
-      System.out.println("#" + z);
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 3; j++) {
-          System.out.print(ans[i][j] + " ");
-        }
-        System.out.println();
-      }
-    }
-  }
-
-  static void rotation() {
-    copy = new int[n][n];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        copy[i][j] = arr[n - j - 1][i];
-      }
-    }
-
-    arr = copy;
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        sb.append(copy[i][j]);
-      }
-      sb.append(" ");
-    }
-  }
+	static int[][] rotationArray(int[][] arr) {
+		int[][] copy = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				copy[i][j] = arr[n - j - 1][i];
+			}
+		}
+		return copy;
+	}
 }
