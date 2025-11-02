152745a82d5901a2d973053d6c07b6e0e274b71b
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-02 11:24:51 +0900

    [D2] Title: 파스칼의 삼각형, Time: 100 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,36 +1,31 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-        
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+	public static void main(String args[]) throws Exception {
 
-        for(int z = 1 ; z <= T ; z ++){
-            int n = sc.nextInt();
-            int[][] arr = new int[n+1][n+1];
-            arr[0][0] = 1;
-
-            for(int i = 1; i <= n; i ++){
-                for(int j = 1; j <= n ; j ++){
-                    arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
-                }
-            }
-            System.out.println("#" + z);
-            for(int i = 1 ; i <= n ; i ++){
-                for(int j = 1 ; j <= n ; j ++){
-                    if(arr[i][j] != 0) {
-                        System.out.print(arr[i][j] + " ");
-                    }
-                }
-                System.out.println();
-            }
-
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int z = 1; z <= T; z++) {
+			int n = sc.nextInt();
+			int[][] arr = new int[n+1][n+1];
+			arr[0][0] = 1;
+			for(int i = 1; i <= n ; i ++) {
+				for(int j = 1; j <= n ; j ++) {
+					arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+				}
+			}
+			
+			System.out.println("#" + z);
+			for(int i = 1; i <= n ; i++) {
+				for(int j = 1 ; j <=n; j++) {
+					if(arr[i][j] != 0) {
+						System.out.print(arr[i][j] + " ");
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
 }
-
-
