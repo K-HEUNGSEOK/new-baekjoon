2e8302c3916da70d11bf1e1a7d88e3a3cfc8a051
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-02 11:45:43 +0900

    [D2] Title: 패턴 마디의 길이, Time: 97 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -1,28 +1,25 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
 import java.util.*;
 
 public class Solution {
-    static char[] temp = new char[10];
-    public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String args[]) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            int ans = 0;
-            String str = sc.next();
-            for(int i = 1 ; i <= 10 ; i ++){
-                String tempA = str.substring(0,i);
-                String tempB = str.substring(i,i*2);
-
-                if (tempA.equals(tempB)){
-                    ans = tempA.length();
-                    break;
-                }
-            }
-            System.out.println("#" + l + " " + ans);
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int z = 1; z <= T; z++) {
+			String str = sc.next();
+			int ans = 0;
+			
+			for(int i = 1; i <= 10; i ++) {
+				String temp = str.substring(0, i);
+				String target = str.substring(i, i * 2);
+				if(target.equals(temp)) {
+					ans = Math.max(ans, temp.length());
+					break;
+				}
+			}
+			System.out.println("#" + z + " " + ans);
+		}
+	}
 }
-
