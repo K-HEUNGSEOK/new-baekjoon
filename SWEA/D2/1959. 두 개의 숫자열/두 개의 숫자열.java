dc5712d518f33ddd84f106714e0d2f6b8b60151a
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-02 11:29:21 +0900

    [D2] Title: 두 개의 숫자열, Time: 103 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -1,46 +1,45 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
 import java.util.*;
 
 public class Solution {
-    static int[] a,b;
-    public static void main(String[] args) throws FileNotFoundException {
-  
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int l = 1 ; l <= T; l ++) {
-            int max = 0;
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            a = new int[n];
-            b = new int[m];
-            for(int i = 0 ; i < n ; i ++) a[i] = sc.nextInt();
-            for(int i = 0 ; i < m ; i ++) b[i] = sc.nextInt();
-
-            //짧은 배열은 무조건 a다
-            if (n > m){
-                int [] tempArr = a;
-                a = b;
-                b = tempArr;
-
-                int temp = n;
-                n = m;
-                m = temp;
-            }
-
-
-            for(int z = 0 ; z <= m - n; z ++){
-                int total = 0;
-                for(int i = 0 ; i < n ; i ++){
-                    total += a[i] * b[i+z];
-                }
-                max  = Math.max(max,total);
-            }
-            //결과 호출
-            System.out.print("#" + l +" ");
-            System.out.println(max);
-        }
-    }
-
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int z = 1; z <= T; z++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int[] a = new int[n];
+			int[] b = new int[m];
+			
+			for(int i = 0 ; i < n ; i ++) {
+				a[i] = sc.nextInt();
+			}
+			for(int i = 0 ; i < m ; i ++) {
+				b[i] = sc.nextInt();
+			}
+			int ans = 0 ;
+			
+			if(n > m) {
+				int temp = n;
+				n = m;
+				m = temp;
+				
+				int[] arr = a;
+				a = b;
+				b = arr;
+			}
+			//여기서 이제 바꿔야한다,
+			for(int i = 0 ; i <= m -n; i ++) {
+				int total = 0;
+				for(int j = 0; j < n ; j ++) {
+					int sum = a[j] * b[j + i];
+					total += sum;
+				}
+				ans = Math.max(ans, total);
+			}
+			System.out.println("#" + z + " " + ans);
+		}
+	}
 }
