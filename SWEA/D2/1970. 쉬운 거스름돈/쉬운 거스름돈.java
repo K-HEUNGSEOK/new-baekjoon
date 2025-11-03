fd2e39a969ee6875c6565b328ca8016610482315
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-03 22:16:07 +0900

    [D2] Title: 쉬운 거스름돈, Time: 156 ms, Memory: 34,048 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,44 +1,41 @@
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
 import java.util.*;
 
 public class Solution {
-    static int[] wallet = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-
-    static int[ ]arr;
-    public static void main(String[] args) throws FileNotFoundException {
-     
-        Scanner sc = new Scanner(System.in);
-
-        //기본 세팅
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            //코드 입력
-           arr = new int[8];
-            int money = sc.nextInt();
-            //배열숫자랑, 총합
-            DFS(0,money);
-            System.out.println("#" + l + " ");
-            for (int i : arr) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }
-
-
-    }
-    static void DFS(int v, int total){
-        if (v >= 8){
-            return;
-        }else{
-            if (total >= wallet[v]){
-                arr[v] = total / wallet[v];
-                DFS(v + 1 , total % wallet[v]);
-            }else{
-                DFS(v + 1 , total);
-            }
-        }
-    }
+	static int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+	static int[] arr;
+	 public static void main(String[] args) throws FileNotFoundException {
+		   
+		    Scanner sc = new Scanner(System.in);
+		    int T = sc.nextInt();
+		    for(int z = 1; z <= T ; z ++) {
+		    	
+		    	//입력
+		    	int n = sc.nextInt();
+		    	arr = new int[8];
+		    	
+		    	
+		    	//주요 로직
+		    	DFS(0, n);
+		    	
+		    	//출력
+		    	System.out.println("#" + z);
+		    	Arrays.stream(arr).forEach(index -> System.out.print(index + " "));
+		    	System.out.println();
+		    }
+	 }
+	 static void DFS(int v, int n) {
+		 if(v == 8) {
+			 return;
+		 }else {
+			if(n >= money[v]) {
+				arr[v] = n / money[v];
+				DFS(v, n % money[v]);
+			}else {
+				DFS(v+1, n);
+			}
+		 }
+	 }
 }
-
