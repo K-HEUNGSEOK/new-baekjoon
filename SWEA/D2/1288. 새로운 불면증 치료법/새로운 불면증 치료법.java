e6a9f21a52810ce3bed84d90cfeb0ea344e61097
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-03 22:39:27 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 107 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,51 +1,46 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-
-        Scanner sc = new Scanner(System.in);
-
-        //기본 세팅
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            int ans = 0;
-            boolean flag = true;
-            int n = sc.nextInt();
-            int[] arr = new int[10];
-            int cnt = 1;
-            while (flag) {
-                int temp = n * cnt;
-             
-                fillArray(arr, temp);
-
-                if (check(arr)) {
-                    ans = temp;
-                    flag = false;
-                }
-
-                cnt ++;
-            }
-            System.out.println("#" + l + " " + ans);
-        }
-    }
-    static boolean check(int [] arr){
-        for(int i = 0; i<= 9; i ++){
-            if (arr[i] <= 0){
-                return false;
-            }
-        }
-        return true;
-    }
-    static void fillArray(int[] arr, int temp){
-        String str = temp + "";
-
-        for(int i = 0 ; i < str.length(); i ++){
-            int z = str.charAt(i) - '0';
-            arr[z] ++;
-        }
-    }
+	static int ans;
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int z = 1; z <= T; z ++) {
+			boolean[] arr = new boolean[10];
+			
+			int n = sc.nextInt();
+			int temp = n;
+			int ans = 0;
+			int cnt= 2;
+			while(true) {
+				ans ++;
+				char[] number = (temp+"").toCharArray();
+				
+				for(char c : number) {
+					int index = c -'0';
+					arr[index] = true;
+				}
+				
+				boolean flag = true;
+				for(int i = 0 ; i < 10; i ++) {
+					if(!arr[i]) {
+						flag = false;
+						break;
+					}
+				}
+				if(flag) {
+					break;
+				}
+				temp = n * cnt;
+				cnt ++;
+			}
+			System.out.println("#" + z + " " + temp);
+		}
+	}
 }
-
