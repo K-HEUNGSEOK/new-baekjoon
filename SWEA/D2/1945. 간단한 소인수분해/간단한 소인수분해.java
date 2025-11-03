a08492ad90e82e58f7c84f4a29b6185abe6d9163
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-03 23:06:51 +0900

    [D2] Title: 간단한 소인수분해, Time: 160 ms, Memory: 33,664 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -1,33 +1,40 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-        
-        Scanner sc = new Scanner(System.in);
+	static int[] number = {2,3,5,7,11};
+	static int[] arr;
+	public static void main(String[] args) throws FileNotFoundException {
 
-        //기본 세팅
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            int n = sc.nextInt();
-            int[] nums = {2,3,5,7,11};
-            int[] temp = new int[5];
-
-                for(int i = 0 ; i < 5; i ++){
-                    while (n % nums[i] == 0){
-                        temp[i] ++;
-                        n /= nums[i];
-                    }
-                }
-            System.out.print("#" + l + " " );
-            for (int i : temp) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int z = 1; z <= T; z ++) {
+			arr = new int[5];
+			int n = sc.nextInt();
+			DFS(0,n);
+			
+			System.out.print("#" + z + " ");
+			Arrays.stream(arr).forEach(num -> System.out.print(num + " "));
+			System.out.println();
+		}
+	}
+	static void DFS(int v, int target) {
+		if(target == 1) {
+			return;
+		}else {
+			if(target % number[v] == 0) {
+				arr[v]++;
+				DFS(v, target / number[v]);
+				
+			}else {
+				DFS(v + 1 , target);
+			}
+		}
+	}
 }
-
