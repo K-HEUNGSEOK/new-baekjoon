d94ab16b912a826492894b98193a2fdacd09a804
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-03 21:56:02 +0900

    [D2] Title: 초심자의 회문 검사, Time: 101 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,31 +1,25 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
 import java.util.*;
 
 public class Solution {
-    static char[] temp = new char[10];
-    public static void main(String[] args) throws FileNotFoundException {
-  
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            boolean flag = true;
-
-            String str = sc.next();
-            for(int i = 0 ; i < str.length(); i ++){
-                if (str.charAt(i) != str.charAt(str.length()-1-i)){
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag){
-                System.out.println("#" + l + " " + 1);
-            }else{
-                System.out.println("#" + l + " " + 0);
-            }
-
-        }
-    }
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int z = 1; z <= T; z++) {
+			String str = sc.next();
+			
+			boolean flag = true;
+			for(int i = 0 ; i < str.length(); i ++) {
+				if(str.charAt(i) != str.charAt(str.length()-1-i)) {
+					flag = false;
+					break;
+				}
+			}
+			
+			System.out.print("#" + z + " ");
+			System.out.println(flag ? 1 : 0);
+		}
+	}
 }
-
