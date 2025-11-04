3fd87a0af961a53b7169f6abfe13c4b5eb46719a
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-04 22:56:14 +0900

    [D2] Title: 시각 덧셈, Time: 99 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -1,31 +1,43 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-       
-        Scanner sc = new Scanner(System.in);
-        //기본 세팅
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            int hoursA = sc.nextInt();
-            int monthA = sc.nextInt();
-            int hoursB = sc.nextInt();
-            int monthB = sc.nextInt();
-
-            int totalA = hoursA * 60 + monthA;
-            int totalB = hoursB * 60 + monthB;
-
-            int temp = totalA + totalB;
-            int ansH = temp / 60;
-            if (ansH > 12) ansH -= 12;
-            int ansM = temp % 60;
-            
-            System.out.println("#" + l + " " + ansH + " " + ansM);
-        }
-    }
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int z = 1 ; z<= T; z++) {
+			int hours1 = sc.nextInt();
+			int minutes1 = sc.nextInt();
+			
+			int hours2 = sc.nextInt();
+			int minutes2 = sc.nextInt();
+			
+			int total = totalMinutes(hours1,minutes1) + totalMinutes(hours2,minutes2);
+			
+			int hours = total / 60;
+			if(hours > 12) {
+				hours -= 12;
+			}
+			int minutes = total % 60;
+			
+			if(minutes > 59) {
+				minutes -= 60;
+				hours ++;
+			}
+		
+			System.out.println("#"+z+ " " + hours + " " + minutes);
+			
+			
+		}
+	}
+	static int totalMinutes(int h, int m) {
+		int total = h *= 60;
+		return total += m;
+	}
 }
-
