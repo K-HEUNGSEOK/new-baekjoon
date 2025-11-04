d25ee4a762da91b8ddb165653c990a05e9b172b2
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-04 22:22:16 +0900

    [D2] Title: 중간 평균값 구하기, Time: 166 ms, Memory: 34,048 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,30 +1,28 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner sc = new Scanner(System.in);
-
-        //기본 세팅
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            int[] arr = new int[10];
-            for(int i = 0 ; i < 10 ; i ++){
-                arr[i] = sc.nextInt();
-            }
-            Arrays.sort(arr);
-            double total = 0 ;
-            for(int i = 1; i < 10-1; i ++){
-                total += arr[i];
-            }
-            int ans = (int) Math.round(total / 8);
-
-            System.out.println("#" + l + " " + ans);
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int z = 1 ; z<= T; z++) {
+			//10개의 수를 입력 받아, 최대 수와 최소 수를 제외한 나머지의 평균값을 출력하는 프로그램을 작성하라.
+			int[] arr = new int[10];
+			for(int i = 0 ; i < 10; i ++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			double ans = Arrays.stream(arr).sorted()
+			.skip(1).limit(8).average().getAsDouble();
+			
+			System.out.println("#" + z + " " + Math.round(ans));
+		}
+	
+	}
 }
-
