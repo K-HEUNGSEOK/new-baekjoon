92d99aec72817b968f2020f5dd0dcdcdb8e9976a
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-06 20:36:13 +0900

    [D2] Title: 가랏! RC카!, Time: 99 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.java
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.java
@@ -1,46 +1,45 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FilterOutputStream;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-  
-        Scanner sc = new Scanner(System.in);
-        //기본 세팅
-        int T = sc.nextInt();
-        for (int l = 1; l <= T; l++) {
-            int time = sc.nextInt(); //총 가야 할 시간
-            int[] arr = new int[35]; //채울 크기
-            int cnt = 0; // 인덱스 추가 양
-            for(int i = 1 ; i <= time; i ++){
-                int command = sc.nextInt();
-                if (command == 0){
-                    arr[i] = cnt;
-                }else if(command == 1){
-                    int speed = sc.nextInt();
-                    cnt += speed;
-
-                    arr[i] = cnt;
-                }else{
-                    int spped = sc.nextInt();
-                    cnt -= spped;
-                    if (cnt < 0){
-                        cnt = 0;
-                    }
-
-                    arr[i] = cnt;
-                }
-            }
-
-
-            int ans = 0;
-            for(int i = 1; i <= time; i ++){
-                ans += arr[i];
-            }
-            System.out.println("#" + l + " " + ans);
-        }
-    }
+	static int speed;
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int z = 1 ; z<= T; z++) {
+			int n = sc.nextInt();
+			int car = 0;
+			speed = 0;
+			
+			for(int i = 0 ; i < n ; i ++) {
+				int index = sc.nextInt();
+				int temp = 0;
+				if(index == 1 || index == 2) {
+					temp = sc.nextInt();
+				}
+				sppedControl(index, temp);
+				car += speed;
+			}
+			System.out.println("#"+z+" " + car);
+		}
+	}
+	static void sppedControl(int index, int spd) {
+		switch(index) {
+		case 1 :
+			speed += spd;
+			break;
+		case 2 :
+			speed -= spd;
+			if(speed < 0) {
+				speed = 0;
+			}
+			break;
+		}
+	}
 }
-
