a3bb8dc95c741e1d572cf1a43d3541190dccc515
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-07 20:06:15 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 159 ms, Memory: 77,936 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,57 +1,52 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
+	static int cnt, total;
+	static char[] arr;
+	static boolean[][] visited ;
+	public static void main(String[] args) throws FileNotFoundException {
 
-  static int ans, cnt;
-  static boolean[][] visited;
-  static char[] arr;
-
-  public static void main(String[] args) throws FileNotFoundException {
-  
-    Scanner sc = new Scanner(System.in);
-    //1. 재귀 완전탐색으로 가장 큰 수를 찾는다
-    //2. 백트래킹으로 다시 되돌려놔야 한다
-    //3. 방문 체크를 해서 가지치기를 하자
-    int T = sc.nextInt();
-    for (int z = 1; z <= T; z++) {
-      String str = sc.next();
-      ans = 0;
-      cnt = sc.nextInt();
-      arr = new char[str.length()];
-      for (int i = 0; i < str.length(); i++) {
-        arr[i] = str.charAt(i);
-      }
-      visited = new boolean[cnt + 1][1000000]; //최대 숫자가 99만임
-      DFS(0, Integer.parseInt(str));
-      System.out.println("#" + z + " " + ans);
-    }
-
-  }
-
-  static void DFS(int v, int total) {
-    if (visited[v][total]) {
-      return;
-    }
-    visited[v][total] = true;
-    if (v == cnt) {
-      ans = Math.max(ans, total);
-      return;
-    } else {
-      for (int i = 0; i < arr.length; i++) {
-        for (int j = i + 1; j < arr.length; j++) {
-          swap(i, j); //스왑하기
-          DFS(v + 1, Integer.parseInt(new String(arr)));
-          swap(i, j); //다시 풀어주기
-        }
-      }
-    }
-  }
-
-  static void swap(int i, int j) {
-    char temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int z = 1 ; z<= T; z++) {
+			//한번씩 다 바꿔보면서 최대값을 찾는 것 
+			
+			int n = sc.nextInt();
+			arr = (n+"").toCharArray();
+			cnt = sc.nextInt();
+			visited = new boolean[cnt+1][1000000];
+			total = 0;
+			DFS(0);
+			System.out.println("#"+z+" " + total);
+		}
+	}
+	static void DFS(int c) {
+		int currentNumber = Integer.parseInt(new String(arr));
+		if(visited[c][currentNumber]) return;
+		
+		visited[c][currentNumber] = true;
+		if(c == cnt) {
+			total = Math.max(total, currentNumber);
+			return;
+		}else {
+			for(int i = 0 ; i < arr.length; i ++) {
+				for(int j = i + 1; j < arr.length; j ++) {
+					char temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+					DFS(c +1);
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	
 }
