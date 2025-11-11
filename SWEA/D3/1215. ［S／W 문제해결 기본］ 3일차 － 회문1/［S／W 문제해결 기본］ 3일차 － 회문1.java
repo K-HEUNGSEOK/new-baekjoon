e72e7feecdf157e94f790117e2f27e0237fc3108
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-11 22:29:43 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 98 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,53 +1,59 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
 
-  public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException {
 
-    Scanner sc = new Scanner(System.in);
-    int T = 10;
-    for (int z = 1; z <= T; z++) {
-      int n = sc.nextInt();
-      char[][] arr = new char[8][8];
-      for (int i = 0; i < 8; i++) {
-        arr[i] = sc.next().toCharArray();
-      }
-      int ans = 0;
-
-      for (int i = 0; i < 8; i++) {
-        for (int j = 0; j <= 8 - n; j++) {
-          StringBuilder sb = new StringBuilder();
-          for (int k = j; k < j + n; k++) {
-            sb.append(arr[i][k]);
-          }
-          if (check(sb.toString())) {
-            ans++;
-          }
-        }
-      }
-      for (int i = 0; i < 8; i++) {
-        for (int j = 0; j <= 8 - n; j++) {
-          StringBuilder sb = new StringBuilder();
-          for (int k = j; k < j + n; k++) {
-            sb.append(arr[k][i]);
-          }
-          if (check(sb.toString())) {
-            ans++;
-          }
-        }
-      }
-      System.out.println("#" + z + " " + ans);
-    }
-  }
-
-  static boolean check(String str) {
-    for (int i = 0; i < str.length(); i++) {
-      if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-        return false;
-      }
-    }
-    return true;
-  }
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+		for (int z = 1; z <= T; z++) {
+			int ans = 0;
+			int n = sc.nextInt();
+			char[][] arr = new char[8][8];
+			for(int i = 0 ; i < 8 ; i ++) {
+				arr[i] = sc.next().toCharArray();
+			}
+			
+			
+			//가로
+			for(int i = 0 ; i < 8 ; i ++) {
+				for(int j = 0 ; j <= 8 - n; j ++) {
+					StringBuilder sb = new StringBuilder();
+					for(int k = j; k < j + n ; k ++) {
+						sb.append(arr[i][k]);
+					}
+					if(isCheck(sb.toString())) {
+						ans ++;
+					}
+				}
+			}
+			//세로
+			for(int i = 0 ; i <= 8 -n ; i ++) {
+				for(int j = 0 ; j < 8; j ++) {
+					StringBuilder sb = new StringBuilder();
+					for(int k = i; k < i + n ; k ++) {
+						sb.append(arr[k][j]);
+					}
+					if(isCheck(sb.toString())) {
+						ans ++;
+					}
+				}
+			}
+			System.out.println("#" + z + " " + ans);
+		}
+	}
+	static boolean isCheck(String sb) {
+		for(int i = 0 ; i < sb.length(); i++) {
+			if(sb.charAt(i) != sb.charAt(sb.length()-i-1)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
