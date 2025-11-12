396d3155f72ef52f84e0a96f844385fd58f453c3
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-12 22:52:30 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 118 ms, Memory: 28,896 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
@@ -1,98 +1,69 @@
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
+	static String[] secret = {"0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111",
+			"0111011", "0110111", "0001011"};
+	public static void main(String[] args) throws FileNotFoundException {
 
-  public static void main(String[] args) throws FileNotFoundException {
-   
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    for (int z = 1; z <= T; z++) {
-      int n = sc.nextInt();
-      int m = sc.nextInt();
-      char[][] arr = new char[n][m];
-      for (int i = 0; i < n; i++) {
-        arr[i] = sc.next().toCharArray();
-      }
-      int x = 0;
-      int y = 0;
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          if (arr[i][j] == '1') {
-            x = i;
-            y = j;
-          }
-        }
-      }
-      StringBuilder sb = new StringBuilder();
-      for (int i = y - 55; i <= y; i++) {
-        sb.append(arr[x][i]);
-      }
-
-      String str = sb.toString();
-      StringBuilder ans = new StringBuilder();
-
-      int len = str.length();
-
-      for (int i = 0; i < len; i += 7) {
-        String temp = str.substring(i, i + 7);
-        ans.append(getNumber(temp));
-      }
-
-      String s = ans.toString();
-      int total = 0;
-      for (int i = 0; i < s.length(); i++) {
-        if (i % 2 == 0) {
-          total += s.charAt(i) - '0';
-        }
-      }
-      total *= 3;
-      for (int i = 0; i < s.length(); i++) {
-        if (i % 2 != 0) {
-          total += s.charAt(i) - '0';
-        }
-      }
-
-      int result = 0;
-      for (int i = 0; i < s.length(); i++) {
-        result += s.charAt(i) - '0';
-      }
-
-      System.out.print("#" + z + " ");
-      if (total % 10 == 0) {
-        System.out.print(result);
-      } else {
-        System.out.print(0);
-      }
-      System.out.println();
-    }
-  }
-
-  static int getNumber(String str) {
-    switch (str) {
-      case "0001101":
-        return 0;
-      case "0011001":
-        return 1;
-      case "0010011":
-        return 2;
-      case "0111101":
-        return 3;
-      case "0100011":
-        return 4;
-      case "0110001":
-        return 5;
-      case "0101111":
-        return 6;
-      case "0111011":
-        return 7;
-      case "0110111":
-        return 8;
-      case "0001011":
-        return 9;
-    }
-    return -1;
-  }
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int z = 1 ; z <= T; z ++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int ans = 0;
+			char[][] arr = new char[n][m];
+			for(int i = 0; i < n; i ++) {
+				arr[i] = sc.next().toCharArray();
+			}
+			//56개 암호코드는 7개 //홀수 자리의 합 *3 + 짝수자리의 합이 10의 배수이면 올바른 암호코드
+			int x = 0;
+			int y = 0;
+			for(int i = 0 ; i < n ; i ++) {
+				for(int j = 0 ; j < m ; j ++) {
+					if(arr[i][j] == '1') {
+						x = i;
+						y = j;
+					}
+				}
+			}
+			
+			int even = 0;
+			int odd = 0;
+			int total = 0;
+			//j 끝에서 56개를 뒤로 가야함
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0 ; i <= y; i ++) {
+				sb.append(arr[x][i]);
+			}
+			String result = sb.toString().substring(y-55, y+1);
+		
+			for(int i = 0 ; i < result.length(); i +=7) {
+				String temp = result.substring(i, i+7);
+				
+				for(int j = 0 ; j < secret.length; j ++) {
+					if(secret[j].equals(temp)) {
+						if((i / 7) % 2 == 0) {
+							odd += j;
+							total += j;
+						}else {
+							even += j;
+							total += j;
+						}
+					}
+				}
+			}
+			odd *= 3;
+		
+			if((odd + even) % 10 == 0) {
+				ans = total;
+			}
+			System.out.println("#" + z + " " + ans);
+		}
+	}
 }
-
