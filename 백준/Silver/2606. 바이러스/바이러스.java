a9c8a7236432b678200f1d93b3ab01f4eedd70b4
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-15 22:26:53 +0900

    [Silver III] Title: 바이러스, Time: 180 ms, Memory: 18116 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,41 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    static int n, m , ans;
-    static boolean[] ch;
-    static ArrayList<ArrayList<Integer>>graph;
-    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        ans = 0;
-        ch = new boolean[n+1];
-        graph= new ArrayList<>();
-        for(int i = 0; i <= n ; i ++){
-            graph.add(new ArrayList<>());
-        }
-        for(int i = 0 ; i < m ; i ++){
-            int a = sc.nextInt();
-            int b= sc.nextInt();
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-        DFS(1);
-        System.out.println(ans);
+  static ArrayList<Integer>[] graph;
+  static boolean[] ch;
+  static int ans;
+
+  public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    graph = new ArrayList[n + 1];
+    for (int i = 0; i <= n; i++) {
+      graph[i] = new ArrayList<>();
     }
-    static void DFS(int v){
-        if (v > n){
-            return;
-        }else{
-            ch[v] = true;
-            for (int nx : graph.get(v)) {
-                if (!ch[nx]){
-                    ans ++;
-                    DFS(nx);
-                }
-            }
-        }
+    for (int i = 0; i < m; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      graph[a].add(b);
+      graph[b].add(a);
     }
+    ch = new boolean[n + 1];
+    ans = 0;
+    DFS(1);
+    System.out.println(ans);
+  }
+
+  static void DFS(int v) {
+    ch[v] = true;
+    for (Integer num : graph[v]) {
+      if (!ch[num]) {
+        ch[num] = true;
+        ans++;
+        DFS(num);
+      }
+    }
+  }
 }
